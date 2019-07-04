f849c498189f579924287c571aa24ac5d77e895c
smo1in <37908645+smo1in@users.noreply.github.com>
2019-07-04 20:11:55 +0300

    Update Router.php

--- components/Router.php
+++ components/Router.php
@@ -23,42 +23,20 @@ class Router
 	public function run()
 	{
 		$uri = $this->getURI();
-
 		foreach ($this->routes as $uriPattern => $path) {
-
 			if(preg_match("~$uriPattern~", $uri)) {
-
-/*				echo "<br>Где ищем (запрос, который набрал пользователь): ".$uri;
-				echo "<br>Что ищем (совпадение из правила): ".$uriPattern;
-				echo "<br>Кто обрабатывает: ".$path; */
-
-				// Получаем внутренний путь из внешнего согласно правилу.
-
 				$internalRoute = preg_replace("~$uriPattern~", $path, $uri);
-
-/*				echo '<br>Нужно сформулировать: '.$internalRoute.'<br>'; */
-
 				$segments = explode('/', $internalRoute);
-
 				$controllerName = array_shift($segments).'Controller';
 				$controllerName = ucfirst($controllerName);
-
-
 				$actionName = 'action'.ucfirst(array_shift($segments));
-
 				$parameters = $segments;
-
-
 				$controllerFile = ROOT . '/controllers/' .$controllerName. '.php';
 				if (file_exists($controllerFile)) {
 					include_once($controllerFile);
 				}
-
 				$controllerObject = new $controllerName;
-				/*$result = $controllerObject->$actionName($parameters); - OLD VERSION */
-				/*$result = call_user_func(array($controllerObject, $actionName), $parameters);*/
 				$result = call_user_func_array(array($controllerObject, $actionName), $parameters);
-				
 				if ($result != null) {
 					break;
 				}
